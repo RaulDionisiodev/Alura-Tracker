6d2557b66ce3f9344e187b92ed2bcb7dacc06850
CORTEX-0241\raul.dionisio_cortex <raul.dionisio@cortex-intelligence.com>
2024-05-28 18:17:12 -0300

    Iniciando o projeto e adicionando o css externo

---
+++ src/components/Formulario.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="box">
+        <div class="columns">
+            <div class="column is-8" role="form" aria-label="Fomrulário para a criação de uma nova tarefa">
+                <input type="text" class="input" placeholder="Qual tarefa vocÊ deseja iniciar?">
+            </div>
+            <div class="column">
+                <div class="is-flex is-align-items-center is-justify-content-space-around">
+                    <section>
+                        <strong>00:00:00</strong>
+                    </section>
+                    <button class="button">
+                        <span class="icon">
+                            <i class="fas fa-play"></i>
+                        </span>
+                        <span>play</span>
+                    </button>
+                    <button class="button">
+                        <span class="icon">
+                            <i class="fas fa-stop"></i>
+                        </span>
+                        <span>stop</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent ({
+    name: 'Formulario'
+})
+
+</script>
