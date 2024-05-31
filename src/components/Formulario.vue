3189663ff28d2412e9596b12ad9425ad8e7d725e
CORTEX-0241\raul.dionisio_cortex <raul.dionisio@cortex-intelligence.com>
2024-05-31 18:42:40 -0300

    Criação da funcionalidade de cronometrar tarefas

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -2,20 +2,22 @@
     <div class="box">
         <div class="columns">
             <div class="column is-8" role="form" aria-label="Fomrulário para a criação de uma nova tarefa">
-                <input type="text" class="input" placeholder="Qual tarefa vocÊ deseja iniciar?">
+                <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?">
             </div>
             <div class="column">
                 <div class="is-flex is-align-items-center is-justify-content-space-around">
                     <section>
-                        <strong>00:00:00</strong>
+                        <strong>
+                            {{ tempoDecorrido }}
+                        </strong>
                     </section>
-                    <button class="button">
+                    <button class="button" @click="iniciarContagem">
                         <span class="icon">
                             <i class="fas fa-play"></i>
                         </span>
                         <span>play</span>
                     </button>
-                    <button class="button">
+                    <button class="button" @click="finalizarContagem">
                         <span class="icon">
                             <i class="fas fa-stop"></i>
                         </span>
@@ -31,7 +33,29 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent ({
-    name: 'Formulario'
+    name: 'Formulario',
+    data () {
+        return {
+            tempoEmSegundos: 0,
+            cronometro: 0
+        }
+    },
+    computed: {
+        //O computed obsera uma informação e, sempre que essa informação for modificada ele atua
+        tempoDecorrido() : string { // uma computed propertie é declarada como uma função
+            return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11,8)
+        }   
+    },
+    methods: {
+        iniciarContagem () {
+            this.cronometro = setInterval(() => {
+                this.tempoEmSegundos += 1
+            }, 1000)
+        },
+        finalizarContagem () {
+            clearInterval(this.cronometro)
+        }
+    }
 })
 
 </script>
