8ebbb1a88399a04798dafccb7b097c5b5660c91f
CORTEX-0241\raul.dionisio_cortex <raul.dionisio@cortex-intelligence.com>
2024-06-12 16:56:40 -0300

    DESAFIO: Extração do botão em um novo componente

---
+++ src/components/Botao.vue
@@ -0,0 +1,56 @@
+<template>
+      <button class="button" @click="manipulaContador" :disabled="cronometroRodando">
+            <span class="icon">
+                <i :class="icon"></i>
+            </span>
+            <span> {{ text }} </span>
+        </button>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+    name: 'Botao',
+    props: { 
+        cronometroRodando : {
+            type: Boolean,
+        }, 
+        icon: {
+            type: String,
+            required: true
+        }, 
+        text: {
+            type: String,
+            required: true
+        }, 
+        type: {
+            type: String,
+            required: true
+        }},
+    emits: ['incia', 'termina'],
+    methods: {
+        manipulaContador() {
+            if (this.type === 'START') {
+                this.$emit('inicia')
+                return
+            }
+
+            if (this.type ===  'STOP') {
+                this.$emit('termina')
+                return
+            }
+            
+        }
+    }
+
+
+
+})
+
+
+
+
+</script>
+
+<style>
+</style>
--- src/components/Temporizador.vue
+++ src/components/Temporizador.vue
@@ -1,29 +1,28 @@
 <template>
     <div class="is-flex is-align-items-center is-justify-content-space-around">
         <cronometro :tempoEmSegundos="tempoEmSegundos" />
-        <button class="button" @click="iniciarContagem" :disabled="cronometroRodando">
-            <span class="icon">
-                <i class="fas fa-play"></i>
-            </span>
-            <span>play</span>
-        </button>
-        <button class="button" @click="finalizarContagem" :disabled="!cronometroRodando">
-            <span class="icon">
-                <i class="fas fa-stop"></i>
-            </span>
-            <span>stop</span>
-        </button>
+    
+        <Botao :cronometroRodando="cronometroRodando" 
+        :icon="'fas fa-play'" :type="'START'" 
+        :text="'play'" @inicia="iniciarContagem" />
+
+        <Botao :cronometroRodando="!cronometroRodando" 
+        :icon="'fas fa-stop'" :type="'STOP'" 
+        :text="'stop'" @termina="finalizarContagem" />
+    
     </div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
 import Cronometro from './Cronometro.vue';
+import Botao from './Botao.vue'
  
 export default defineComponent({
     name: 'Temporizador',
     emits: ['aoTemporizadorFinalizado'],
     components: {
-        Cronometro
+        Cronometro,
+        Botao
     },
     data () {
         return {
