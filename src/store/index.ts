e7731f1b151c620e697f5d75bc027873bf9ad75a
CORTEX-0241\raul.dionisio_cortex <raul.dionisio@cortex-intelligence.com>
2024-08-01 18:19:00 -0300

    Exibindo projetos cadastrados na página de tarefas

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,6 @@
 import IProjeto from "@/interfaces/IProjeto";
 import { InjectionKey } from "vue";
-import { Store, createStore } from "vuex";
+import { Store, createStore, useStore as vuexUseStore } from "vuex";
 
 interface Estado {
     projetos: IProjeto[]
@@ -11,5 +11,19 @@ export const key: InjectionKey<Store<Estado>> = Symbol()
 export const store = createStore<Estado>({
     state : {
         projetos: []
+    },
+    mutations: { //funções que podem adicionar coisas ao estado
+        'ADICIONA_PROJETO'(state, nomeDoProjeto: string){
+            const projeto = {
+                id: new Date().toISOString(),
+                nome: nomeDoProjeto
+            } as IProjeto
+
+            state.projetos.push(projeto)
+        }
     }
 })
+
+export function useStore() : Store<Estado> {
+    return vuexUseStore(key)
+}
